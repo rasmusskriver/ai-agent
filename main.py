9048e5c74bee960dff67a1c56944e0de33a45cc0
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-11-08 22:45:14 +0100

    lavede det til en function

--- main.py
+++ main.py
@@ -3,22 +3,37 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
-    if len(sys.argv) <= 1:
-        print("skriv noget forsatan")
-        sys.exit(1)
-
     load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=sys.argv[1],
+        contents=messages,
     )
+
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
