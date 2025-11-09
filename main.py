8aff11076ae0d50b43f4b4fb93ec3b88d50acf93
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-11-09 13:27:30 +0100

    rettet ting til for at ligne eksempel mere

--- main.py
+++ main.py
@@ -9,41 +9,44 @@ from google.genai import types
 def main():
     load_dotenv()
 
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = []
+
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-
-    verbose_tjek = "--verbose" in args
-
-    if verbose_tjek:
-        args.remove("--verbose")
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     user_prompt = " ".join(args)
 
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    generate_content(client, messages, verbose_tjek, user_prompt)
+    generate_content(client, messages, verbose)
 
 
-def generate_content(client, messages, verbose, user_prompt):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
-
-    print(response.text)
     if verbose:
-        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
