d6379b6a6d78aa6dd4dea801f08c4a85e84a2c78
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-11-12 22:10:04 +0100

    ændret så det er som i undervisnings filen

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -1,5 +1,7 @@
 import os
 
+from config import MAX_CHARS
+
 
 def get_file_content(working_directory, file_path):
     working_directory_path = os.path.abspath(working_directory)
@@ -8,12 +10,11 @@ def get_file_content(working_directory, file_path):
         return f"Error: Cannot read {working_directory_path} as it is outside the permitted working directory"
     if not os.path.isfile(target_dir):
         return f"Error: File not found or is not a regular file: {target_dir}"
-
-    if os.path.getsize(target_dir) >= 10000:
+    try:
         with open(target_dir) as file_yes:
-            content = file_yes.read(10000)
-        return f"{content}...File {target_dir} truncated at 10000 characters"
-
-    with open(target_dir) as file_yes:
-        content = file_yes.read()
-        return f"{content}"
+            content = file_yes.read(MAX_CHARS)
+            if os.path.getsize(target_dir) > MAX_CHARS:
+                content += f'[...File "{target_dir}" truncated at {MAX_CHARS} characters]'
+        return content
+    except Exception as e:
+        return f'Error reading file "{target_dir}": {e}'
