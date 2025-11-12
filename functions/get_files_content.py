154668ca10f03351bb3b03e0ccc3eb30761b79aa
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-11-12 21:52:55 +0100

    endnu en function tilføjet

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -1,2 +1,19 @@
+import os
+
+
 def get_file_content(working_directory, file_path):
-    pass
+    working_directory_path = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_dir.startswith(working_directory_path):
+        return f"Error: Cannot read {working_directory_path} as it is outside the permitted working directory"
+    if not os.path.isfile(target_dir):
+        return f"Error: File not found or is not a regular file: {target_dir}"
+
+    if os.path.getsize(target_dir) >= 10000:
+        with open(target_dir) as file_yes:
+            content = file_yes.read(10000)
+        return f"{content}...File {target_dir} truncated at 10000 characters"
+
+    with open(target_dir) as file_yes:
+        content = file_yes.read()
+        return f"{content}"
--- tests.py
+++ tests.py
@@ -1,23 +1,11 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_content import get_file_content
 
 
 def test():
-    result = get_files_info("calculator", ".")
-    print("Result for current directory:")
-    print(result)
-    print("")
-
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
-    print(result)
-
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin' directory:")
-    print(result)
-
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
-    print(result)
+    print(get_file_content("calculator", "main.py"))
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(get_file_content("calculator", "/bin/cat"))
+    print(get_file_content("calculator", "pkg/does_not_exist.py"))
 
 
 if __name__ == "__main__":
