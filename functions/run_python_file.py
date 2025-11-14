aa2dd0eb83e786b294fc3840258951360f89933e
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-11-15 00:31:13 +0100

    har lavet punkt 1 t.o.m. 4 i run python opgaven

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -4,5 +4,9 @@ import os
 def run_python_file(working_directory, file_path, args=[]):
     abs_working_directory = os.path.abspath(working_directory)
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
-    print(f"abs_working_directory: {abs_working_directory}")
-    print(f"abs_file_path: {abs_file_path}")
+    if not abs_file_path.startswith(abs_working_directory):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
