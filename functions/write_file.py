902e8233d810b7909348128a9067a796f9775a5d
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-11-14 22:16:02 +0100

    tilføjet ekstra tjek der er i solution files

--- functions/write_file.py
+++ functions/write_file.py
@@ -2,12 +2,19 @@ import os
 
 
 def write_file(working_directory, file_path, content):
-    working_directory_path = os.path.abspath(working_directory)
-    target_dir = os.path.abspath(os.path.join(working_directory, file_path))
-    if not target_dir.startswith(working_directory_path):
-        return f"Error: Cannot read {working_directory_path} as it is outside the permitted working directory"
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f"Error: Cannot read {file_path} as it is outside the permitted working directory"
+    if not os.path.exists(abs_file_path):
+        try:
+            os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
+        except Exception as e:
+            return f"Error: creating directory: {e}"
+    if os.path.exists(abs_file_path) and os.path.isdir(abs_file_path):
+        return f'Error: "{file_path}" is a directory, not a file'
     try:
-        with open(target_dir, "w") as f:
+        with open(abs_file_path, "w") as f:
             f.write(content)
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
