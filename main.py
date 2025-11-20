c34a5c052d519b068510cfbaf95778a21bfa6c19
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-11-20 18:00:42 +0100

    tilføjet en system prompt

--- main.py
+++ main.py
@@ -33,13 +33,15 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    generate_content(client, messages, verbose)
+    system_prompt = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT'"
+    generate_content(client, messages, verbose, system_prompt)
 
 
-def generate_content(client, messages, verbose):
+def generate_content(client, messages, verbose, system_prompt):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
