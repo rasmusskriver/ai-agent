437f9ef36eb3e5cb884bb81d7a6009a1a9c89209
Rasmus Skriver <rasmus_skriver@yahoo.dk>
2025-11-20 18:38:01 +0100

    nu kan generative agent modellen kalde på functions

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+# from functions.get_files_info import schema_get_files_info
 from prompts import system_prompt
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
 
 
 def main():
@@ -39,17 +54,27 @@ def main():
 
 
 def generate_content(client, messages, verbose):
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
     )
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
     print("Response:")
-    print(response.text)
+    # print(response)
+    if response.function_calls is None:
+        print(response.text)
+    else:
+        for func in response.function_calls:
+            print(func)
 
 
 if __name__ == "__main__":
